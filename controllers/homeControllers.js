c70e0d4f8e96609e6a740db74c9b9da2d57027e3
Kanhaiya Kumar <kanhaiya09042005@gmail.com>
2026-07-13 12:04:02 +0530

    Add total expenses dashboard summary

--- controllers/homeControllers.js
+++ controllers/homeControllers.js
@@ -15,7 +15,33 @@ async function getHomePage(req, res){
         return res.status(500).send("Internal Server Error");
     }
 }
+
+const Expense = require("../models/expenseModel");
+
+async function handleHome(req, res) {
+    try {
+
+        const expenses = await Expense.find({
+            createdBy: req.user._id,
+        });
+
+        const totalExpenses = expenses.reduce(
+            (total, expense) => total + expense.amount,
+            0
+        );
+
+        return res.render("home", {
+            expenses,
+            totalExpenses,
+        });
+
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).send("Internal Server Error");
+    }
+}
+
 module.exports = {
     getHomePage,
-    
+    handleHome,
 }
